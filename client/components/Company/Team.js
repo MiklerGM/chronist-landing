497d4b2ca0323fcd6e30574075aad31713f4b99e
Dmitriy Shikhaleev <padavankun@gmail.com>
2018-01-10 18:19:16 +0300

    fix team img link to nowhere

--- client/components/Company/Team.js
+++ client/components/Company/Team.js
@@ -4,9 +4,7 @@ import data from './data/TeamData';
 
 const Person = ({ person }) => (
   <div className='person--data'>
-    <a href={person.link}>
-      <img src={person.img} />
-    </a>
+    <img src={person.img} />
     <p>
       <strong> {person.name} </strong>
     <br />
